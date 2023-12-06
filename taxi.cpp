9ec590710ebda3c213616e9de9b9a4f8a627969d
MazaArt <84805964+MazaArt@users.noreply.github.com>
2023-12-06 10:59:07 -0500

    Added taxi problem - solved

--- taxi.cpp
+++ taxi.cpp
@@ -5,8 +5,22 @@ int main()
 {
     int number;
     cin >> number;
-    while (number > 0){
-
-        number--;
+    int count[5] = {0}; 
+    int c;
+    while (number--){
+        cin >> c;
+        count[c]++;
     }
+    int counter = count[4] + count[3] + count[2]/2;
+    count[1] -= count[3];
+    if (count[2] % 2 == 1) {
+        count[1] -= 2;
+        counter++;
+    }
+    if(count[1] > 0){
+        counter += (count[1] + 3) / 4;
+        // add 3 to account for round down
+    }
+    cout << counter;
+    return 0;
 }
